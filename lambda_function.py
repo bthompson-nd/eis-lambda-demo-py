972ddde0077828fa5c42c54eb0696bda556e80ae
Ben Thompson <ben.thompson@nd.edu>
2022-08-31 17:30:34 -0400

    and now lambda_function is formatted with black

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,7 @@
 from lambda_types import LambdaDict, LambdaContext
 import json
 
+
 def lambda_handler(event: LambdaDict, context: LambdaContext) -> LambdaDict:
     # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}

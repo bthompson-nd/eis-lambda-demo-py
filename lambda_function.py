65a462bb4ed0c86feb649aebaacf50c07d8c2300
Ben Thompson <ben.thompson@nd.edu>
2022-09-01 09:40:16 -0400

    pylint: ignore the lambda_types.py, lambda_function: syntax error

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,5 @@ from lambda_types import LambdaDict, LambdaContext
 
 def lambda_handler(event: LambdaDict, context: LambdaContext) -> LambdaDict:
     '''The main Lambda handler. The entry point for your logic.'''
-    return {"statusCode": 200, "body": json.dumps(dict(event=event, context=context)}
+    return {"statusCode": 200, "body": json.dumps(dict(event=event, context=context))}
 
